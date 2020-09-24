72a31d9dd40350c177991492e11fcdb2232744c0
Sven Greb <development@svengreb.de>
2020-09-24 09:21:31 +0200

    Go stub package and test
    To prevent `golangci-lint` and the CI workflow `test` job from failing
    a stub file for the `tmplgo` package has been added along with a example
    test (`tmplgo_test` package).
    This also comes with the benefit of providing a simple starting point
    for users of this template repository.
    
    GH-12

---
+++ tmpl-go_test.go
@@ -0,0 +1,14 @@
+// Copyright (c) 2020-present Sven Greb <development@svengreb.de>
+// This source code is licensed under the MIT license found in the LICENSE file.
+
+package tmplgo_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestStub(t *testing.T) {
+	require.True(t, true)
+}
